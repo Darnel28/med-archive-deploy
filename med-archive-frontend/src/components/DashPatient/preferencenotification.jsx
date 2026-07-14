e167f02e2f496f14d5b681f8ed5ab8c550e0c37e
Priscillia <liliesperancia28@gmail.com>
2026-07-14 15:37:04 +0100

    Les Navlinks

--- med-archive-frontend/src/components/DashPatient/preferencenotification.jsx
+++ med-archive-frontend/src/components/DashPatient/preferencenotification.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import NavLink from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { FiUser, FiSettings, FiLock, FiBell } from 'react-icons/fi';
 const PreferencesNotifications = () => {
   // État des toggles
