3662792c0f504b17cf8285aa983d7bd3ca083c68
Priscillia <liliesperancia28@gmail.com>
2026-07-22 17:17:54 +0100

    Nouveauurr fresh

--- med-archive-backend/entrypoint.sh
+++ med-archive-backend/entrypoint.sh
@@ -4,7 +4,7 @@ php artisan config:clear
 php artisan cache:clear
 
 php artisan migrate --force
-#  php artisan db:seed --force
- php artisan migrate:fresh --seed --force
+ php artisan db:seed --force
+#  php artisan migrate:fresh --seed --force
 
 exec php artisan serve --host=0.0.0.0 --port=${PORT}
